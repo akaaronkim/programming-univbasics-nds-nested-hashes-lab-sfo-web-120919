a6965f06f457c7b3fffde63b8781aac013509e4d
Aaron Kim <akaaronkim@gmail.com>
2019-11-24 01:19:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,13 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  assembled = [
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
-    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-    KATE_G = { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
-    ]
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def literal_aoh
